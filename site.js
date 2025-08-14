ebba2223bf70ce1918f29857ae90450a16f1b1c5
AndrewNguyenM <147673445+AndrewNguyenM@users.noreply.github.com>
2025-08-14 01:56:08 -0400

    Delete site.js

--- site.js
+++
@@ -1,7 +0,0 @@
-
-<script>
-  // Footer year
-  const yEl = document.getElementById('y'); if (yEl) yEl.textContent = new Date().getFullYear();
-  const y2El = document.getElementById('y2'); if (y2El) y2El.textContent = new Date().getFullYear();
-  const y3El = document.getElementById('y3'); if (y3El) y3El.textContent = new Date().getFullYear();
-</script>
