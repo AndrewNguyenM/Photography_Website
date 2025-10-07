e57f49658f643dd8544612c05f851bc59bdcf750
Andrew Nguyen <nguye938@msu.edu>
2025-10-07 14:44:26 -0400

    Added gr 86

--- data.js
+++ data.js
@@ -11,7 +11,8 @@ window.PROJECTS = [
   { slug:"ford-gt",  collection:"automotive", title:"2017 Ford GT", year:2024, location:"Backyard", blurb:"Golden hour", cover:"images/automobile/ford_gt/wheel-lg.jpg" },
   { slug:"2001-honda-s2000", collection:"automotive", title:"Honda S2000", year:2025, location:"Troy", blurb:"S2000 - Kaushik Paladugu", cover:"images/automobile/s2000/IMG_0090-2.jpg" },
   { slug:"city-cafe",         collection:"lifestyle",  title:"City Café", year:2023, location:"River North", blurb:"Quiet morning light.", cover:"https://images.unsplash.com/photo-1517245386807-bb43f82c33c4?q=80&w=1600&auto=format&fit=crop" },
-  { slug:"yamaha-r3", collection:"automotive", title:"Yamaha R3", year:2025, location:"Sterling Heights", blurb:"Yamaha R3 - Musanna Khan", cover:"images/automobile/Yamaha_R3/bike.jpg" }
+  { slug:"yamaha-r3", collection:"automotive", title:"Yamaha R3", year:2025, location:"Sterling Heights", blurb:"Yamaha R3 - Musanna Khan", cover:"images/automobile/Yamaha_R3/bike.jpg" },
+  { slug:"gr86", collection:"automotive", title:"GR86", year:2025, location:"Troy", blurb:"GR86 - Andrew Nguyen", cover:"images/automobile/gr86/wide-GR86.jpg"}
 ];
 
 //INSIDE EACH WINDOWED BOX
@@ -160,8 +161,45 @@ window.PHOTOS = {
         focalLength: "50mm"
       }
     },
-
     ],
+
+  "gr86": [
+    {
+      scrSmall: "images/automobile/gr86/wide-GR86.jpg",
+      srcLarge: "images/automobile/gr86/wide-GR86.jpg",
+      w: 1259, h: 2000,
+      title: "GR86",
+      story: "My GR86",
+      meta: {
+        date: "2025-10-07",
+        location: "Park",
+        camera: "Canon T7",
+        lens: "RF 50mm f/1.8",
+        shutter: "1/80s",
+        aperture: "f/2.0",
+        iso: "800",
+        focalLength: "50mm"
+      }
+    },
+    {
+      scrSmall: "images/automobile/gr86/rear-GR86",
+      srcLarge: "images/automobile/gr86/rear-GR86",
+      w: 1259, h: 2000,
+      title: "GR86",
+      story: "GR86 Rear",
+      meta: {
+        date: "2025-10-07",
+        location: "Park",
+        camera: "Canon T7",
+        lens: "RF 50mm f/1.8",
+        shutter: "1/80s",
+        aperture: "f/2.0",
+        iso: "800",
+        focalLength: "50mm"
+      }
+    },
+  ],
+  
   "city-cafe": [
     {
       srcSmall:"https://images.unsplash.com/photo-1517245386807-bb43f82c33c4?q=80&w=800&auto=format&fit=crop",
@@ -171,4 +209,5 @@ window.PHOTOS = {
       meta:{ date:"2023-04-02", location:"River North", camera:"X100V", lens:"23mm", shutter:"1/125s", aperture:"f/2", iso:"320", focalLength:"23mm" }
     }
   ]
+
 };
